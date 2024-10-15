ccbad547a6e51a2745e3c7d86d3c608237c9be31
shri-acha <happy.irhs@gmail.com>
2024-10-15 17:51:32 +0545

    More specific scraping with formatted output and formatted data

--- main.go
+++ main.go
@@ -2,51 +2,67 @@ package main
 
 import (
 	"fmt"
-	// "io"
 	"net/http"
+	"strings"
 	"golang.org/x/net/html"
 )
 
-
 type Link struct {
-	linkURL string;
-	isInternal bool;
-} 
+	linkURL    string
+	isInternal bool
+}
 
-func main(){
-
-
+func main() {
 	var link string
-	fmt.Printf("Enter a (static) website to scrape for links:")
-	
+	fmt.Printf("Enter a (static)website to scrape for links: ")
 
-	fmt.Scanf("%s",&link)
+	fmt.Scanf("%s", &link)
 	fmt.Printf("Starting the webscraper...\n")
 
-
-	resp,err := http.Get(link)
-
-
-	if err != nil{
-		fmt.Printf("An Error Occured!: %v",err)
+	resp, err := http.Get(link)
+	if err != nil {
+		fmt.Printf("An Error Occured!: %v\n", err)
 		return
 	}
-
 	defer resp.Body.Close()
 
-	// body,err := io.ReadAll(resp.Body)
-	
-
-	z := html.NewTokenizer(resp.Body)
-
-	for{
-
-  if z.Next() == html.ErrorToken {
-		return
+	var tags []string
+	tags = append(tags, "a")
+	scrapedLinks := printScraped(tags, resp,link)	
+	for i := range scrapedLinks {
+		fmt.Printf("Link:%v\tIs-Internal:%v\n",scrapedLinks[i].linkURL,scrapedLinks[i].isInternal)
 	}
-	tempByteString,_ := z.TagName()
-		if string(tempByteString) == "a" {
-		fmt.Printf("Result Body:%v\n",string(z.Raw()))
+}
+
+func printScraped(links []string, resp *http.Response,link string) []Link{
+	z := html.NewTokenizer(resp.Body)
+	var tempLinkArrBuff []Link
+	for {
+		tokenType := z.Next()
+		if tokenType == html.ErrorToken {
+			break
+		}
+
+		tempByteString, _ := z.TagName()
+
+		for _, linkTag := range links {
+			if string(tempByteString) == linkTag {
+				for {
+					key, value, moreAttr := z.TagAttr()
+					if string(key) == "href" {
+						tempValue := string(value)
+					if len(tempValue) > 0 && (tempValue[0] == '/' || strings.Contains(link, tempValue)) {
+						tempLinkArrBuff = append(tempLinkArrBuff, Link{tempValue, true})
+					}else {
+						tempLinkArrBuff = append(tempLinkArrBuff, Link{tempValue, false})
+						}
+					}
+					if !moreAttr {
+						break
+					}	
+				}
+			}
 		}
 	}
+	return tempLinkArrBuff
 }
