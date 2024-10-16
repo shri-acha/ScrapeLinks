f442b7cbd75a2d5db60f7fee2cd6b4bdd6c67d0c
shri-acha <happy.irhs@gmail.com>
2024-10-16 08:04:54 +0545

    Introduced go-routines for concurrent processing of links

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"strings"
+	"sync"
 	"golang.org/x/net/html"
 )
 
@@ -13,28 +15,50 @@ type Link struct {
 }
 
 func main() {
-	var link string
-	fmt.Printf("Enter a (static)website to scrape for links: ")
-
-	fmt.Scanf("%s", &link)
-	fmt.Printf("Starting the webscraper...\n")
-
-	resp, err := http.Get(link)
-	if err != nil {
-		fmt.Printf("An Error Occured!: %v\n", err)
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "At least 1 argument is required!\nSyntax: ./main -u [URL]")
 		return
 	}
-	defer resp.Body.Close()
-
-	var tags []string
-	tags = append(tags, "a")
-	scrapedLinks := printScraped(tags, resp,link)	
-	for i := range scrapedLinks {
-		fmt.Printf("Link:%v\tIs-Internal:%v\n",scrapedLinks[i].linkURL,scrapedLinks[i].isInternal)
+	if (os.Args[1] == "-u") && (len(os.Args[2]) > 0) {
+		links := os.Args[2:]
+		var wg sync.WaitGroup
+		results := make(chan []Link, len(links))
+		for _, link := range links {
+			wg.Add(1)
+			go func(link string) {
+				defer wg.Done()
+				printScraped(link, results)
+			}(link)
+		}
+		go func() {
+			wg.Wait()
+			close(results)
+		}()
+		for scrapedLinks := range results {
+			for _, link := range scrapedLinks {
+				fmt.Printf("Link: %v\tIs-Internal: %v\n", link.linkURL, link.isInternal)
+			}
+		}
+	} else {
+		fmt.Printf("Syntax: ./main -u [LINK]")
 	}
 }
 
-func printScraped(links []string, resp *http.Response,link string) []Link{
+func printScraped(link string, results chan<- []Link) {
+	resp, err := http.Get(link)
+	if err != nil {
+		fmt.Printf("An error occurred while fetching %s: %v\n", link, err)
+		results <- []Link{}
+		return
+	}
+	defer resp.Body.Close()
+	var tags []string
+	tags = append(tags, "a")
+	fmt.Println("Scraped links for the website:\t" + link)
+	scrapeLinks(tags, resp, link, results)
+}
+
+func scrapeLinks(links []string, resp *http.Response, baseLink string, results chan<- []Link) {
 	z := html.NewTokenizer(resp.Body)
 	var tempLinkArrBuff []Link
 	for {
@@ -42,27 +66,22 @@ func printScraped(links []string, resp *http.Response,link string) []Link{
 		if tokenType == html.ErrorToken {
 			break
 		}
-
 		tempByteString, _ := z.TagName()
-
 		for _, linkTag := range links {
 			if string(tempByteString) == linkTag {
 				for {
 					key, value, moreAttr := z.TagAttr()
 					if string(key) == "href" {
 						tempValue := string(value)
-					if len(tempValue) > 0 && (tempValue[0] == '/' || strings.Contains(link, tempValue)) {
-						tempLinkArrBuff = append(tempLinkArrBuff, Link{tempValue, true})
-					}else {
-						tempLinkArrBuff = append(tempLinkArrBuff, Link{tempValue, false})
-						}
+						isInternal := len(tempValue) > 0 && (tempValue[0] == '/' || strings.Contains(tempValue, baseLink))
+						tempLinkArrBuff = append(tempLinkArrBuff, Link{tempValue, isInternal})
 					}
 					if !moreAttr {
 						break
-					}	
+					}
 				}
 			}
 		}
 	}
-	return tempLinkArrBuff
+	results <- tempLinkArrBuff
 }
